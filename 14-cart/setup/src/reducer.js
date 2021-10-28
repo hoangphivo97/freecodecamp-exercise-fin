11958ed68fedb2ff17d848288fa0e1fe73d3a63a
hoangphivo97 <phivo1397@gmail.com>
2021-10-28 16:42:27 +0700

    fetch data from API

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -29,6 +29,29 @@ const reducer = (state,action) =>{
         return {...state,cart:tempCart}
         
     }
+    if(action.type === 'GET_TOTALS'){
+        let {total,amount}  = state.cart.reduce((cartTotal,cartItem)=>{
+            const {price,amount} = cartItem
+            const totalPrice = price * amount;
+            cartTotal.total += totalPrice
+            cartTotal.amount += amount;
+            
+            return cartTotal
+        },{
+            total:0,
+            amount:0,
+        }
+        )
+        total = parseFloat(total.toFixed(2))
+        
+        return {...state,total,amount}
+    }
+    if(action.type === 'LOADING'){
+        return ({...state, loading:true})
+    }
+    if(action.type === 'DISPLAY_ITEMS'){
+        return ({...state,cart:action.payload,loading:false})
+    }
     return state
 }
 
